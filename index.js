58f2543ef7b1c15f20180ca2c76a16c131568a8c
unknown <francisco.ramirez@tecsup.edu.pe>
2020-10-15 10:17:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+// inicio index clientes proveedores contactos etc
+app.get('/', c_inicio);
+app.get('/saludo', c_saludo);
+
+function c_inicio(req,res)
+{
+    res.send('<h1> Index en Express </h1>');
+}
+function c_saludo(req,res)
+{
+    res.send('<h1> Hola Francisco </h1>');
+}
+function c_server()
+{
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log(host + ' : '+port);
+}
+
+var server = app.listen(8000,c_server);
